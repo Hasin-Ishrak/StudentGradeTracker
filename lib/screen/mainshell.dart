35d385674924cac01466556eb7d24774ed7bab95
Hasin-Ishrak <ishrakhasin758@gmail.com>
2026-06-30 22:13:55 +0600

    added screenlogic to show

---
+++ lib/screen/mainshell.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/themeprovider.dart';
+import 'add_subject_screen.dart';
+import 'subject_list_screen.dart';
+import 'summary_screen.dart';
+
+class MainShell extends StatelessWidget {
+  const MainShell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final selectedIndex = ValueNotifier<int>(0);
+    final screens = [
+      const AddSubjectScreen(),
+      const SubjectListScreen(),
+      const SummaryScreen(),
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Student Grade Tracker'),
+        actions: [
+          Consumer<ThemeProvider>(
+            builder: (context, themeProvider, _) {
+              return IconButton(
+                icon: Icon(
+                  themeProvider.isDarkMode
+                      ? Icons.light_mode
+                      : Icons.dark_mode,
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+                onPressed: () => themeProvider.toggleTheme(),
+                tooltip: 'Toggle theme',
+              );
+            },
+          ),
+        ],
+      ),
+      body: ValueListenableBuilder<int>(
+        valueListenable: selectedIndex,
+        builder: (context, index, _) => screens[index],
+      ),
+      bottomNavigationBar: ValueListenableBuilder<int>(
+        valueListenable: selectedIndex,
+        builder: (context, index, _) {
+          return BottomNavigationBar(
+            currentIndex: index,
+            onTap: (newIndex) => selectedIndex.value = newIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_box_outlined),
+                label: 'Add',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list_alt),
+                label: 'Subjects',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bar_chart),
+                label: 'Summary',
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
